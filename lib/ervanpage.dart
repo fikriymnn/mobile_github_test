09faf22b6675c04b41a91ab7608f3c80c3978243
Astarith <oktamuhamad30@gmail.com>
2024-08-07 10:26:17 +0700

    menambah listview

---
+++ lib/ervanpage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Ervanpage extends StatefulWidget {
+  const Ervanpage({super.key});
+
+  @override
+  State<Ervanpage> createState() => _ErvanpageState();
+}
+
+class _ErvanpageState extends State<Ervanpage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
